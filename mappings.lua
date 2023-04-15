370eacbeecd9640599f166a3649122f0fbbccd28
Nikita Levashov <sh4rpy@ya.ru>
2023-04-16 01:32:12 +0300

    накинул комментов

--- mappings.lua
+++ mappings.lua
@@ -1,7 +1,8 @@
- -- Custom mappings config
+ -- Кастомные бинды.
 
 return {
   n = {
+    -- Переключения между открытыми файлами через Shift+l/Shift+h вместо неудобного ]b/[b.
     ["<S-l>"] = {
       function()
         require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
