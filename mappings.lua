5a2f3f432e52ea9e3626053abd45d50acb9bdc97
Nikita Levashov <sh4rpy@ya.ru>
2023-04-16 01:12:45 +0300

    кастомные бинды для переключения между открытыми файлами

---
+++ mappings.lua
@@ -0,0 +1,18 @@
+ -- Custom mappings config
+
+return {
+  n = {
+    ["<S-l>"] = {
+      function()
+        require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+      end,
+      desc = "Next buffer",
+    },
+    ["<S-h>"] = {
+      function()
+        require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+      end,
+      desc = "Previous buffer",
+    },
+  },
+}
